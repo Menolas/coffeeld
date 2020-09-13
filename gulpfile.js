da6cd5f0c832771285f7f0053862aa898ef9365b
Menolas <olenakunina@gmail.com>
2020-09-13 11:22:41 +0200

    automatisation

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var posthtml = require("gulp-posthtml");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var include = require("posthtml-include");
+var del = require("del");
+var server = require("browser-sync").create();
+
+
+gulp.task("style", function() {
+  return gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream())
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "./",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", gulp.series("style"));
+  gulp.watch("*.html").on("change", server.reload);
+});
